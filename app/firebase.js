3cc11452db1771a5c91466460e1e0c93fb250ef2
Twilight <158759143+Muneeb-Ahmad-404@users.noreply.github.com>
2024-07-31 12:28:27 +0500

    git remote add origin https://github.com/Muneeb-Ahmad-404/pantry-tracker.git git branch -M main git push -u origin main

---
+++ app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyCnV527zrcQOQksxP4FJOJ9E2RPbyFHbCU",
+authDomain: "pantry-app-251d0.firebaseapp.com",
+projectId: "pantry-app-251d0",
+storageBucket: "pantry-app-251d0.appspot.com",
+messagingSenderId: "815707738654",
+appId: "1:815707738654:web:78d4d22226799246ad940f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
